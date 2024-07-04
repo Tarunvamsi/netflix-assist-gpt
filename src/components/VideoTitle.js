74ff92e881b384886a2420ec082a2d49b5b62056
PUSARLA TARUN VAMSI <pusarlatarunvamsi@gmail.com>
2024-07-04 10:40:52 +0530

    Arranged icons in main container for mobile responsive

--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -6,17 +6,17 @@ import UnmuteIcons from "./icons/UnmuteIcons";
 
 const VideoTitle = ({ title, overview, isMuted, toggleMute }) => {
   return (
-    <div className="w-screen aspect-video pt-[17%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black">
+    <div className="w-screen aspect-video pt-[29%] md:pt-[17%] px-6 md:px-24 absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-2xl md:text-6xl font-bold">{title}</h1>
       <p className="hidden md:inline-block py-6 text-md w-1/4">{overview}</p>
       <div className="my-4 flex">
-        <button className="bg-red-500 text-white py-1 md:py-4 px-4 md:px-10 rounded-lg text-md md:text-xl opacity-70 hover:bg-opacity-80 ">
+        <button className="bg-red-500 text-white py-1 md:py-4 px-4 md:px-10 rounded-lg text-md md:text-xl opacity-70 hover:bg-opacity-80 h-10 md:h-auto">
           <div className="flex items-center justify-between h-6">
             <PlayIcon></PlayIcon> Play
           </div>
         </button>
 
-        <button className="inline-block mx-2 bg-gray-500 text-white p-4 px-10 text-lg rounded-lg opacity-70 hover:bg-opacity-80 ">
+        <button className="inline-block mx-2 bg-gray-500 text-white py-1 md:py-4 px-2 md:px-10 text-base md:text-lg rounded-lg opacity-70 hover:bg-opacity-80 h-10 md:h-auto w-18 md:w-auto ">
           <div className="flex items-center justify-between h-6">
             <InfoCircleIcon></InfoCircleIcon> More info
           </div>
