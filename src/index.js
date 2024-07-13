4b1d1bb2838eede1a10d669a30d1e0c706488442
PUSARLA TARUN VAMSI <pusarlatarunvamsi@gmail.com>
2024-07-13 15:52:40 +0530

    Enabled vercel analytics in index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { inject } from "@vercel/analytics";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+inject();
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
